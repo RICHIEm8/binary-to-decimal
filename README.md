833a9a6b85ea302ed2791476c2b1b0f995d1deb7
Bryan Lee <bryanrichie97@gmail.com>
2021-02-22 14:34:45 +1100

    feat(app): added conditional statements to ensure only binary digits are entered and cleaned up result displaying

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## binary to decimal converter written in typescript
+
+![Alt text](src/binary-to-decimal-screenshot-1.png 'binary-to-decimal screenshot 1')
+![Alt text](src/binary-to-decimal-screenshot-2.png 'binary-to-decimal screenshot 2')
--- src/App.tsx
+++ src/App.tsx
@@ -7,38 +7,50 @@ import {
   NumberInputField,
   Text,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 function App() {
   const [query, setQuery] = useState('');
   const [result, setResult] = useState('');
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     const binToDec = parseInt(query.toString(), 2);
-    setResult(binToDec.toString());
-    setQuery('');
+    const checkForBinary = query.match(/^[0-1]+$/);
+    if (!checkForBinary) {
+      setResult('Please make sure you only enter digits 0 or 1!');
+    } else {
+      setResult(`${query} = ${binToDec.toString()}`);
+      setQuery('');
+    }
   };
 
   return (
-    <Flex flexDirection="column" alignItems="center" bgColor="green.500" minHeight="100vh">
-      <Heading as="h1" color="white">
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      bgColor="blue.500"
+      minHeight="100vh"
+      justifyContent="center"
+    >
+      <Heading as="h1" size="4xl" color="white">
         Binary to Decimal
       </Heading>
-      <Flex justifyContent="center">
+      <Flex justifyContent="center" mt={50} mb={25}>
         <HStack spacing={0}>
           <form onSubmit={handleSubmit}>
-            <NumberInput>
-              <NumberInputField
+            <Flex>
+              <NumberInput
                 w={412}
-                maxlength="8"
                 placeholder="Enter binary code..."
                 bgColor="white"
-                onChange={(e) => setQuery(e.target.value)}
+                onChange={setQuery}
                 value={query}
-              />
-            </NumberInput>
-            <Button type="submit">Submit</Button>
+              >
+                <NumberInputField maxLength={8} />
+              </NumberInput>
+              <Button type="submit">Submit</Button>
+            </Flex>
           </form>
         </HStack>
       </Flex>
